86ad2ec1397b58f782b3baeb222f2d5d757c8c1d
Giraphi <hoeps.raphael@funct.com>
2024-02-27 14:43:38 +0100

    feature: save filter status in url

--- app/Filters.tsx
+++ app/Filters.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
 import { cn, hasCommonElement } from "@/lib/utils";
 import { CatData } from "../types/types";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { parseAsArrayOf, parseAsString, useQueryState } from "nuqs";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 const filterBreeds = [
     { id: "beng", name: "Bengal" },
@@ -17,27 +18,42 @@ export interface FiltersProps {
 }
 
 export default function Filters(props: FiltersProps) {
-    const [activeBreeds, setActiveBreeds] = useState<string[]>([]);
+    const [activeBreeds, setActiveBreeds] = useQueryState("breeds", parseAsArrayOf(parseAsString));
+    const [isFirstRender, setIsFirstRender] = useState(true);
 
-    function onBreedsChange(breedIds: string[]) {
-        const filteredData = !breedIds.length
-            ? props.data
-            : props.data.filter((cat: CatData) => {
+    function filterData(data: CatData[], breedIds: string[]) {
+        return !breedIds || !breedIds.length
+            ? data
+            : data.filter((cat: CatData) => {
                   return hasCommonElement(
                       cat.breeds.map((b) => b.id),
                       breedIds,
                   );
               });
-        setActiveBreeds(breedIds);
+    }
+
+    function onBreedsChange(breedIds: string[]) {
+        const filteredData = filterData(props.data, breedIds);
+        const activeBreeds = !!breedIds && breedIds.length ? breedIds : null;
+        setActiveBreeds(activeBreeds);
         props.onDataFiltered(filteredData);
     }
+
+    useEffect(() => {
+        if (!isFirstRender || !activeBreeds) {
+            return;
+        }
+        setIsFirstRender(false);
+        const filteredData = filterData(props.data, activeBreeds);
+        props.onDataFiltered(filteredData);
+    }, [activeBreeds, isFirstRender, props]);
 
     return (
         <div className={cn("pointer-events-none fixed bottom-10 flex w-full justify-center", props.className)}>
             <div className="pointer-events-auto  flex rounded-lg bg-neutral-200 p-1 shadow shadow-neutral-500">
                 <ToggleGroup
                     type="multiple"
-                    value={activeBreeds}
+                    value={activeBreeds || []}
                     onValueChange={(breedIds) => onBreedsChange(breedIds)}
                 >
                     {filterBreeds.map((breed) => (
